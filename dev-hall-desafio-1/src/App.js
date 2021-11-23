4ed9a754dd97efdfd711a5a405f313bf7a00af4a
Hamander <1hamander@gmail.com>
2021-11-23 18:15:00 -0300

    build(hero/css): Hero created

--- dev-hall-desafio-1/src/App.js
+++ dev-hall-desafio-1/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Header } from './components';
-
+import { Header, Hero } from './components';
+import './styles/App.css'
 function App() {
   return(
-    <>
+    <div className="main">
       <Header />
-    </>
+      <Hero />
+    </div>
   );
 }
 
---
+++ dev-hall-desafio-1/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Hero.css';
+function Hero() {
+  return (
+    <section className="hero-image">
+      <div className="hero-text">
+        <p>Desenvolvedores prontos para ação</p>
+        <h1>Seu projeto pronto na velocidade da luz</h1>
+        <button>Peça uma reunião</button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
